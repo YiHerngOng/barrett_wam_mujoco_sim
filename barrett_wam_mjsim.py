1d59e4ea840a99f78a83b7ce64baa417aabccebd
YiHerngOng <ongyi@oregonstate.edu>
2018-12-13 11:42:36 -0800

    able to actuate robot arm and hand

--- barrett_wam_mjsim.py
+++ barrett_wam_mjsim.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python
 #<!--frictional="true" frictionloss=".001"-->
-from mujoco_py import load_model_from_path, MjSim, MjViewer
+from mujoco_py import load_model_from_path, MjSim, MjViewer, MjSimState
 import math
 import os
+import random
+import numpy as np
 
 MODEL_XML = 'wam_7dof_wam_bhand.xml'
-# MODEL_XML = '/home/yiherngong/barrett/bhand.xml'
 
-# model = MjModel(MODEL_XML)
+def extract_pos():
+	wam_base_yaw = random.uniform(-2.6, 2.6)
+	wam_shoulder_pitch = random.uniform(-1.985,1.985)
+	wam_shoulder_yaw = random.uniform(-2.8, 2.8)
+	wam_elbow_pitch = random.uniform(-0.9, 3.14159)
+	wam_wrist_yaw = random.uniform(-4.55, 1.25)
+	wam_wrist_pitch = random.uniform(-1.5707, 1.5707)
+	wam_palm_yaw = random.uniform(-3,3)
 
-# model._compute_subtree()
+	bhand_f1p = random.uniform(0, 3.14159) # spread
+	bhand_f1m = random.uniform(0, 2.44346)
+	bhand_f1d = random.uniform(0, 0.837758)
+	bhand_f2p = random.uniform(0, 3.14159)
+	bhand_f2m = random.uniform(0, 2.44346)
+	bhand_f2d = random.uniform(0, 0.837758)
+	bhand_f3m = random.uniform(0, 2.44346)
+	bhand_f3d = random.uniform(0, 0.837758)
+
+	return np.array([wam_base_yaw, wam_shoulder_pitch, wam_shoulder_yaw, wam_elbow_pitch, wam_wrist_yaw, wam_wrist_pitch, wam_palm_yaw, bhand_f1p, bhand_f1m, bhand_f1d, bhand_f2p, bhand_f2m, bhand_f2d, bhand_f3m, bhand_f3d])
+
+# def PD_controller():
+
+
 model = load_model_from_path(MODEL_XML)
 sim = MjSim(model)
 viewer = MjViewer(sim)
+# state = MjSimState()
 t = 0
 while True:
     # sim.data.ctrl[0] = math.cos(t / 10.) * 0.01
     # sim.data.ctrl[1] = math.sin(t / 10.) * 0.01
     t += 1
+    # sim.forward()
     sim.step()
     viewer.render()
+    sim.data.ctrl[:] = extract_pos()
     # if t>1000:
    	# 	break
 

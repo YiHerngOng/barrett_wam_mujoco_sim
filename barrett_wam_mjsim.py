2e832807442e42063af53215883a4233b20c9149
YiHerngOng <ongyi@oregonstate.edu>
2018-12-19 21:01:12 -0800

    done path planning

--- barrett_wam_mjsim.py
+++ barrett_wam_mjsim.py
@@ -5,26 +5,49 @@ import math
 import os
 import random
 import numpy as np
-
-MODEL_XML = 'wam_7dof_wam_bhand.xml'
+import time
+MODEL_XML = '/home/graspinglab/barrett_wam_mujoco_sim/wam_7dof_wam_bhand.xml'
 
 def extract_pos():
-	wam_base_yaw = random.uniform(-2.6, 2.6)
-	wam_shoulder_pitch = random.uniform(-1.985,1.985)
-	wam_shoulder_yaw = random.uniform(-2.8, 2.8)
-	wam_elbow_pitch = random.uniform(-0.9, 3.14159)
-	wam_wrist_yaw = random.uniform(-4.55, 1.25)
-	wam_wrist_pitch = random.uniform(-1.5707, 1.5707)
-	wam_palm_yaw = random.uniform(-3,3)
+	wam_base_yaw = 0
+	wam_shoulder_pitch = 0
+	wam_shoulder_yaw = 0
+	wam_elbow_pitch = 0
+	wam_wrist_yaw = 0
+	wam_wrist_pitch = 0
+	wam_palm_yaw = 0
 
-	bhand_f1p = random.uniform(0, 3.14159) # spread
-	bhand_f1m = random.uniform(0, 2.44346)
-	bhand_f1d = random.uniform(0, 0.837758)
-	bhand_f2p = random.uniform(0, 3.14159)
-	bhand_f2m = random.uniform(0, 2.44346)
-	bhand_f2d = random.uniform(0, 0.837758)
-	bhand_f3m = random.uniform(0, 2.44346)
-	bhand_f3d = random.uniform(0, 0.837758)
+	bhand_f1p = 0 # spread
+	bhand_f1m = 0
+	bhand_f1d = 0
+	bhand_f2p = 0
+	bhand_f2m = 0
+	bhand_f2d = 0
+	bhand_f3m = 0
+	bhand_f3d = 0
+
+
+	return np.array([wam_base_yaw, wam_shoulder_pitch, wam_shoulder_yaw, wam_elbow_pitch, wam_wrist_yaw, wam_wrist_pitch, wam_palm_yaw, bhand_f1p, bhand_f1m, bhand_f1d, bhand_f2p, bhand_f2m, bhand_f2d, bhand_f3m, bhand_f3d])
+
+
+def extract_pos1():
+	wam_base_yaw = 1.5777
+	wam_shoulder_pitch = 1.1511
+	wam_shoulder_yaw = 0
+	wam_elbow_pitch = 0.94315678
+	wam_wrist_yaw = -3.1276419
+	wam_wrist_pitch = 0.5235
+	wam_palm_yaw = 1.5587
+
+	bhand_f1p = 0 # spread
+	bhand_f1m = 0
+	bhand_f1d = 0
+	bhand_f2p = 0
+	bhand_f2m = 0
+	bhand_f2d = 0
+	bhand_f3m = 0
+	bhand_f3d = 0
+
 
 	return np.array([wam_base_yaw, wam_shoulder_pitch, wam_shoulder_yaw, wam_elbow_pitch, wam_wrist_yaw, wam_wrist_pitch, wam_palm_yaw, bhand_f1p, bhand_f1m, bhand_f1d, bhand_f2p, bhand_f2m, bhand_f2d, bhand_f3m, bhand_f3d])
 
@@ -32,12 +55,18 @@ def extract_pos():
 
 
 model = load_model_from_path(MODEL_XML)
-sim = MjSim(model)
-viewer = MjViewer(sim)
+# sim = MjSim(model)
+# viewer = MjViewer(sim)
 # state = MjSimState()
 t = 0
-while True:
-    t += 1
-    sim.step()
-    viewer.render()
-    sim.data.ctrl[:] = extract_pos()
+# while True:
+#     t += 1
+#     sim.step()
+#     viewer.render()
+#     # sim.data.ctrl[:] = extract_pos()
+#     # time.sleep(10)
+#     sim.data.ctrl[:] = extract_pos1()
+f = open("test1.csv", "r")
+traj = f.readlines()
+for i in traj:
+	print(i.split(',')) 
